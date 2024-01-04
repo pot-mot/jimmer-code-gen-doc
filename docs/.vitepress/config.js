d55a31ad48f0b00330e0933414bddf45727471c9
Potmot <377029227@qq.com>
2024-01-04 16:50:11 +0800

    vitepress config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'Jimmer Code Gen 文档',
+    description: '旨在方便快速的创建 Jimmer 下实体与关联',
+    base: '/jimmer-code-gen-doc/',
+
+    themeConfig: {
+        sidebar: [
+            {
+                text: '快速开始',
+                link: '/index.html'
+            },
+            { 
+                text: '前端', 
+                link: '/frontend/index.html' 
+            },
+            { 
+                text: '后端', 
+                link: '/backend/index.html' 
+            },
+        ]
+    }
+}
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,3 @@
 # Jimmer Code Gen
 
-旨在方便快速的创建 Jimmer 下的实体
+旨在方便快速的创建 Jimmer 下实体与关联
